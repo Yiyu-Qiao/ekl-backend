efc27f33b7b1470011c1359c190817b8f159cdd7
QiaoHanDev <timhanq@hotmail.com>
2022-04-17 13:23:54 +0200

    Initial commit

---
+++ src/main/java/de/han/ekl/backend/EklBackendApplication.java
@@ -0,0 +1,13 @@
+package de.han.ekl.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EklBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EklBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/han/ekl/backend/EklBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.han.ekl.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EklBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
