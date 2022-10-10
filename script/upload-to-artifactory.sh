593490a252aba584ead79e0ef330d10c67d8b338
QiaoHanDev <timhanq@hotmail.com>
2022-10-10 22:58:56 +0200

    modify variable reference

--- script/upload-to-artifactory.sh
+++ script/upload-to-artifactory.sh
@@ -4,5 +4,5 @@ set +x
 
 curl_return_code = $(curl -H "Authorization: Bearer ${ARTIFACTORY_TOKEN}" -X PUT "https://qiaohandev.jfrog.io/artifactory/ekl-snapshot-local/de/han/ekl/backend/ekl-backend-0.0.1-SNAPSHOT.jar" -T ./target/ekl-backend-0.0.1-SNAPSHOT.jar)
 
-echo $curl_return_code
+echo ${curl_return_code}
 sh 'exit 1'
