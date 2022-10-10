34d92a1c0d6f7e473798ffe6010414db19e43206
QiaoHanDev <timhanq@hotmail.com>
2022-10-10 22:50:35 +0200

    read curl output

--- script/upload-to-artifactory.sh
+++ script/upload-to-artifactory.sh
@@ -2,6 +2,7 @@
 
 set +x
 
-curl -H "Authorization: Bearer ${ARTIFACTORY_TOKEN}" -X PUT "https://qiaohandev.jfrog.io/artifactory/ekl-snapshot-local/de/han/ekl/backend/ekl-backend-0.0.1-SNAPSHOT.jar" -T ./target/ekl-backend-0.0.1-SNAPSHOT.jar
+curl_return_code = $(curl -H "Authorization: Bearer ${ARTIFACTORY_TOKEN}" -X PUT "https://qiaohandev.jfrog.io/artifactory/ekl-snapshot-local/de/han/ekl/backend/ekl-backend-0.0.1-SNAPSHOT.jar" -T ./target/ekl-backend-0.0.1-SNAPSHOT.jar)
 
+echo $curl_return_code
 sh 'exit 1'
