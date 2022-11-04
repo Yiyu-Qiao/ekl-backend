02b6e3b5c88ec53ffb5088d8cd7312f3252e2623
qiao.han@allianz.de <qiao.han@allianz.de>
2022-11-04 19:33:53 +0100

    cat artifactory-response-header.txt

--- script/upload-to-artifactory.sh
+++ script/upload-to-artifactory.sh
@@ -5,4 +5,5 @@ set +x
 curl_return_code=$(curl --dump-header artifactory-response-header.txt -H "Authorization: Bearer ${ARTIFACTORY_TOKEN}" -X PUT "https://qiaohandev.jfrog.io/artifactory/ekl-snapshot-local/de/han/ekl/backend/ekl-backend-0.0.1-SNAPSHOT.jar" -T ./target/ekl-backend-0.0.1-SNAPSHOT.jar)
 
 echo "cur return code : $curl_return_code"
-sh 'exit 1'
+
+cat artifactory-response-header.txt
