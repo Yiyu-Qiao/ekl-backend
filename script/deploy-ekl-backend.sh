48cd1dcf2668d4bae2e36b2bc78348fbe7f79a51
Qiao Han <timhanq@hotmail.com>
2022-11-05 20:57:35 +0100

    echo return-code-scp

--- script/deploy-ekl-backend.sh
+++ script/deploy-ekl-backend.sh
@@ -2,4 +2,5 @@
 
 echo $idjenkinsuser
 cat $idjenkinsuser
-scp -i $idjenkinsuser ./target/ekl-backend-0.0.1-SNAPSHOT.jar jenkins-user@192.168.178.62:/home/jenkins-user/tmp/ekl-backend/
+return-code-scp = $(scp -i $idjenkinsuser ./target/ekl-backend-0.0.1-SNAPSHOT.jar jenkins-user@192.168.178.62:/home/jenkins-user/tmp/ekl-backend/)
+echo $return-code-scp
