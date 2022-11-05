f050233b66a3b6286290eebb30805748a6f9a51f
Qiao Han <timhanq@hotmail.com>
2022-11-05 21:22:16 +0100

    cat pk

--- script/deploy-ekl-backend.sh
+++ script/deploy-ekl-backend.sh
@@ -4,5 +4,5 @@ echo $idjenkinsuser
 cp $idjenkinsuser ./pk-jenkins-user.txt
 chmod u=rw,go-rwx ./pk-jenkins-user.txt
 cat $idjenkinsuser
-return_code_scp=$(scp -i ./pk-jenkins-user.txt ./target/ekl-backend-0.0.1-SNAPSHOT.jar jenkins-user@192.168.178.62:/home/jenkins-user/tmp/ekl-backend/)
-echo $return_code_scp
+cat pk-jenkins-user.txt
+scp -i pk-jenkins-user.txt ./target/ekl-backend-0.0.1-SNAPSHOT.jar jenkins-user@192.168.178.62:/home/jenkins-user/tmp/ekl-backend/
