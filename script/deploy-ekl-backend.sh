4e0418c69cc3e524cb638837a704b7ff9a7ffba3
Qiao Han <timhanq@hotmail.com>
2022-11-05 21:17:25 +0100

    cp used

--- script/deploy-ekl-backend.sh
+++ script/deploy-ekl-backend.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
 echo $idjenkinsuser
+cp $idjenkinsuser ./pk-jenkins-user.txt
+chmod u=rw,go-rwx ./pk-jenkins-user.txt
 cat $idjenkinsuser
-return_code_scp=$(scp -i $idjenkinsuser ./target/ekl-backend-0.0.1-SNAPSHOT.jar jenkins-user@192.168.178.62:/home/jenkins-user/tmp/ekl-backend/)
+return_code_scp=$(scp -i ./pk-jenkins-user.txt ./target/ekl-backend-0.0.1-SNAPSHOT.jar jenkins-user@192.168.178.62:/home/jenkins-user/tmp/ekl-backend/)
 echo $return_code_scp
