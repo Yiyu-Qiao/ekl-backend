8b97404adf138b7ec31bf8b522d8ebebedd4303d
Qiao Han <timhanq@hotmail.com>
2022-11-05 20:52:45 +0100

    cat $idjenkinsuser

--- script/deploy-ekl-backend.sh
+++ script/deploy-ekl-backend.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
 echo $idjenkinsuser
+cat $idjenkinsuser
 scp -i $idjenkinsuser ./target/ekl-backend-0.0.1-SNAPSHOT.jar jenkins-user@192.168.178.62:/home/jenkins-user/tmp/ekl-backend/
