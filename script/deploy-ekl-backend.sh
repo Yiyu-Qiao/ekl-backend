81c50f9461c594c5871e6e532e09141bdd765e5f
Qiao Han <timhanq@hotmail.com>
2022-11-05 21:03:23 +0100

    underscore usded

--- script/deploy-ekl-backend.sh
+++ script/deploy-ekl-backend.sh
@@ -2,5 +2,5 @@
 
 echo $idjenkinsuser
 cat $idjenkinsuser
-return-code-scp=$(scp -i $idjenkinsuser ./target/ekl-backend-0.0.1-SNAPSHOT.jar jenkins-user@192.168.178.62:/home/jenkins-user/tmp/ekl-backend/)
-echo $return-code-scp
+return_code_scp=$(scp -i $idjenkinsuser ./target/ekl-backend-0.0.1-SNAPSHOT.jar jenkins-user@192.168.178.62:/home/jenkins-user/tmp/ekl-backend/)
+echo $return_code_scp
