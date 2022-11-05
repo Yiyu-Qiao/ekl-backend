d1ab5e72c747e88fb1a52a6dc11a6c85f901df14
Qiao Han <timhanq@hotmail.com>
2022-11-05 20:42:45 +0100

    scp used

--- script/deploy-ekl-backend.sh
+++ script/deploy-ekl-backend.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
 
 echo $idjenkinsuser
-ssh -i $idjenkinsuser jenkins-user@192.168.178.62
-id
-hostname
+scp -i $idjenkinsuser ./target/ekl-backend-0.0.1-SNAPSHOT.jar jenkins-user@192.168.178.62:/home/jenkins-user/tmp/ekl-backend/
