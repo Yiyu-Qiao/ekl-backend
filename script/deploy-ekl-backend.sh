9cd52335c1f3cea6983bc5757695368f3df74df3
qiao.han@allianz.de <qiao.han@allianz.de>
2022-11-05 19:58:30 +0100

    $ as variable used2

--- script/deploy-ekl-backend.sh
+++ script/deploy-ekl-backend.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-echo ${id-jenkins-user}
-ssh -i ${id-jenkins-user} jenkins-user@192.168.178.62
+echo $id-jenkins-user
+ssh -i $id-jenkins-user jenkins-user@192.168.178.62
 id
 hostname
