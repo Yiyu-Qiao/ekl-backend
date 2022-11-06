f83274aad34c16df97d7aad85944c3a5db99b52e
Qiao Han <timhanq@hotmail.com>
2022-11-06 20:46:00 +0100

    refactoring

--- script/upload-to-artifactory.sh
+++ script/upload-to-artifactory.sh
@@ -2,7 +2,7 @@
 
 set +x
 
-#curl_return_code=$(curl --dump-header artifactory-response-header.txt -H "Authorization: Bearer ${ARTIFACTORY_TOKEN}" -X PUT "https://qiaohandev.jfrog.io/artifactory/ekl-snapshot-local/de/han/ekl/backend/ekl-backend-0.0.1-SNAPSHOT.jar" -T ./target/ekl-backend-0.0.1-SNAPSHOT.jar)
+curl_return_code=$(curl --dump-header artifactory-response-header.txt -H "Authorization: Bearer ${ARTIFACTORY_TOKEN}" -X PUT "https://qiaohandev.jfrog.io/artifactory/ekl-snapshot-local/de/han/ekl/backend/ekl-backend-0.0.1-SNAPSHOT.jar" -T ./target/ekl-backend-0.0.1-SNAPSHOT.jar)
 
 echo $ARTIFACTORY_TOKEN
 
